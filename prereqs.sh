d5a5f05c51d3a10abb537c0530e549e2a6b2b371
vladmd22 <109072881+vladmd22@users.noreply.github.com>
2023-10-25 13:57:15 +0000

    modified

--- prereqs.sh
+++ prereqs.sh
@@ -1,6 +1,1 @@
-# install lolcat
-git clone https://github.com/jaseg/lolcat.git 
 
-# install cowsay
-git clone https://github.com/cowsay-org/cowsay.git
-rm ./cowsay/Makefile && mv Makefile_cowsay ./cowsay/Makefile
